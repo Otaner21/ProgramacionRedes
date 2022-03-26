924da58b7381aabfa5d2afb7b69ea9a94870d61c
Otaner21 <renatods2121@gmail.com>
2022-03-26 13:01:20 -0300

    Clase de practico de Programacion Redes, corregir las ip en el codigo

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+            socketServer.Bind(localEndpoint);
+            socketServer.Listen(1);
+            while (true)
+            {
+                var socketClient = socketServer.Accept();
+                Console.WriteLine("Acepete un nuevo perdido de conexion");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/SocketClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Startiing CLient Aplication...");
+            var socketClient= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("1.0.0.1"), 20000);
+            var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
+            socketClient.Bind(localEndpoint);
+            socketClient.Connect(remoteEndpoint);
+            Console.WriteLine("Connected to server!!");
+            Console.ReadLine();
+        }
+    }
+}
