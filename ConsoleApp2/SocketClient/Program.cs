90ce82c081195f8ce359270ad70eda93b536cf48
Otaner21 <renatods2121@gmail.com>
2022-03-26 13:26:44 -0300

    conexion cliente servidor

--- ConsoleApp2/SocketClient/Program.cs
+++ ConsoleApp2/SocketClient/Program.cs
@@ -10,7 +10,7 @@ namespace SocketClient
         {
             Console.WriteLine("Startiing CLient Aplication...");
             var socketClient= new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            var localEndpoint = new IPEndPoint(IPAddress.Parse("1.0.0.1"), 20000);
+            var localEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 15000);
             var remoteEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000);
             socketClient.Bind(localEndpoint);
             socketClient.Connect(remoteEndpoint);
