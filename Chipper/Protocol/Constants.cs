182025b96cf542a3b9acea206b9198de91b3f889
Otaner21 <renatods2121@gmail.com>
2022-04-04 20:18:40 -0300

    Ejemplo de clase 4-4

--- Chipper/Protocol/Constants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Protocol{}
-
-public static class Constants
-{
-    public const int FixedLength = 4;
-}
---
+++ Ejemplo Clas/ProtocolLibrary/HeaderConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace ProtocolLibrary
+{
+    public static class HeaderConstants
+    {
+        public static string Request = "REQ";
+        public static string Response = "RES";
+        public static int CommandLength = 2;
+        public static int DataLength = 4;
+    }
+}
+
+// Envio del client al server -> REQXXYYYY -> REQ -> sentido de la operacion
+//                                                -> XX -> Numero de Comando (00-99)
+//                                                -> YYYY -> 0000 - 9999
